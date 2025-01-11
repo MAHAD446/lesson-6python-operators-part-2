e5abac24da0a9ee2005904ffc1e5b1d2be10ebbc
mahad446 <mahadmubashir43@gmail.com>
2025-01-11 14:27:24 -0800

    activity#1

---
+++ activity#1.py
@@ -0,0 +1,29 @@
+a = 10
+b = 12
+c = 0
+if a>c and b>c:
+    print("all the numbers have boolen value as true")
+else:
+    print("at least one number has boolen value as false")
+
+
+
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+
+if b > 0 or c > 0:
+    print("either of the number is greater than 0")
+
+else:
+    print("no number is greater than 0")
+
+
